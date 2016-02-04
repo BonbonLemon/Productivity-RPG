85446c4c58c8d2d3f163224014acbe0558382a15
Stephen Chen <chen.stephen.r@gmail.com>
2016-02-04 13:52:10 -0800

    active done buttons implemented

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -34089,27 +34089,82 @@
 	var TaskType = React.createClass({
 	  displayName: 'TaskType',
 
+	  getInitialState: function () {
+	    return {
+	      activeSelected: true,
+	      doneSelected: false
+	    };
+	  },
+
+	  selectActive: function () {
+	    if (this.state.doneSelected) {
+	      this.setState({
+	        activeSelected: true,
+	        doneSelected: false
+	      });
+	    }
+	  },
+
+	  selectDone: function () {
+	    if (this.state.activeSelected) {
+	      this.setState({
+	        activeSelected: false,
+	        doneSelected: true
+	      });
+	    }
+	  },
+
 	  render: function () {
+	    var typeName = this.props.taskType.type_name;
+	    var activeDoneButtons;
+	    if (typeName === "To-dos") {
+	      var activeStyle, doneStyle;
+	      if (this.state.activeSelected) {
+	        activeStyle = "selected";
+	      }
+	      if (this.state.doneSelected) {
+	        doneStyle = "selected";
+	      }
+	      activeDoneButtons = React.createElement(
+	        'div',
+	        { className: 'row' },
+	        React.createElement(
+	          'ul',
+	          { className: 'task-filter col-xs-10 col-xs-offset-1' },
+	          React.createElement(
+	            'li',
+	            { className: "active filter col-xs-6 " + activeStyle, onClick: this.selectActive },
+	            'Active'
+	          ),
+	          React.createElement(
+	            'li',
+	            { className: "done filter col-xs-6 " + doneStyle, onClick: this.selectDone },
+	            'Done'
+	          )
+	        )
+	      );
+	    }
 	    return React.createElement(
 	      'div',
-	      { className: "task-type-container col-xs-3 " + this.props.taskType.type_name },
+	      { className: "task-type-container col-xs-3 " + typeName },
 	      React.createElement(
 	        'div',
 	        { className: 'task-type col-xs-12' },
 	        React.createElement(
 	          'h2',
 	          { className: 'task-type-name' },
-	          this.props.taskType.type_name
+	          typeName
 	        ),
 	        React.createElement(TaskForm, { taskType: this.props.taskType }),
+	        activeDoneButtons,
 	        React.createElement(
 	          'ul',
 	          { className: 'task-box row' },
-	          this.props.taskType.tasks.map(function (task, idx) {
-	            if (!task.completed) {
+	          this.props.taskType.tasks.map((function (task, idx) {
+	            if (task.completed === this.state.doneSelected) {
 	              return React.createElement(Task, { key: idx, task: task });
 	            }
-	          })
+	          }).bind(this))
 	        )
 	      )
 	    );
--- frontend/components/taskType.jsx
+++ frontend/components/taskType.jsx
@@ -3,21 +3,66 @@ var React = require('react'),
     Task = require('./task');
 
 var TaskType = React.createClass({
+  getInitialState: function () {
+    return ({
+      activeSelected: true,
+      doneSelected: false
+    });
+  },
+
+  selectActive: function () {
+    if (this.state.doneSelected) {
+      this.setState({
+        activeSelected: true,
+        doneSelected: false
+      });
+    }
+  },
+
+  selectDone: function () {
+    if (this.state.activeSelected) {
+      this.setState({
+        activeSelected: false,
+        doneSelected: true
+      });
+    }
+  },
+
   render: function () {
+    var typeName = this.props.taskType.type_name;
+    var activeDoneButtons;
+    if (typeName === "To-dos") {
+      var activeStyle, doneStyle;
+      if (this.state.activeSelected) {
+        activeStyle = "selected"
+      }
+      if (this.state.doneSelected) {
+        doneStyle = "selected"
+      }
+      activeDoneButtons = (
+        <div className="row">
+          <ul className="task-filter col-xs-10 col-xs-offset-1">
+            <li className={"active filter col-xs-6 " + activeStyle} onClick={this.selectActive}>Active</li>
+            <li className={"done filter col-xs-6 " + doneStyle} onClick={this.selectDone}>Done</li>
+          </ul>
+        </div>
+      )
+    }
     return (
-      <div className={"task-type-container col-xs-3 "+ this.props.taskType.type_name}>
+      <div className={"task-type-container col-xs-3 "+ typeName}>
         <div className="task-type col-xs-12">
-          <h2 className="task-type-name">{this.props.taskType.type_name}</h2>
+          <h2 className="task-type-name">{typeName}</h2>
           <TaskForm taskType={this.props.taskType} />
+          {activeDoneButtons}
           <ul className="task-box row">
             {
               this.props.taskType.tasks.map(function (task, idx) {
-                if (!task.completed) {
+                if (task.completed === this.state.doneSelected) {
                   return (
                     <Task key={idx} task={task}/>
                   );
                 }
-              })
+              }.bind(this))
             }
           </ul>
         </div>
